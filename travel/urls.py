732fb625baddb189819f3380153a00613e24c35b
Alexandr <domarevsasha@gmail.com>
2023-01-09 12:01:20 +0300

    New app "routes"

--- travel/urls.py
+++ travel/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home, about
+from routes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
-    path('about/', about)
+
 ]
